1446171ec19d298987a880a6f5cb4124f43f7e5a
Gabriel-Alves-TI <gabriel.alvesantos213@gmail.com>
2024-11-25 22:59:36 -0300

    ajustado server.js para aceitar requisições externas

--- back-end/server.js
+++ back-end/server.js
@@ -2,4 +2,7 @@ const http = require('http');
 const app = require('./app');
 const port = process.env.PORT || 3000;
 const server = http.createServer(app);
-server.listen(port);
+
+server.listen(port, '0.0.0.0', () => {
+    console.log(`Servidor está rodando em HTTP://0.0.0.0:${port}`);
+});
